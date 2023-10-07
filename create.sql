7740f0788220598a70d2c642a4b6554e58a482e6
Adrielle-Dantas-27 <Adrielledsilva@gmail.com>
2023-10-07 13:12:28 -0300

    added comments

--- create.sql
+++ create.sql
@@ -1,23 +1,24 @@
-create database escola;
+create database escola; --  cria um novo banco de dados do zero
 
-use escola;
+use escola; -- entra no banco de dados ja criado
 
-create table cliente (
-id_cliente int primary key,
+-- cria uma nova tabela   
+create table cliente (             
+id_cliente int primary key,        -- aqui são as especificações das colunas
 nome varchar(100)
 );
 
 create table curso (
-id_curso int primary key,
-nomecurso varchar(100),
-valor decimal(10,2)
+id_curso int primary key,          -- primary key = chave primária
+nomecurso varchar(100),            -- varchar é para caracter/ string  ||  o char vai ocupar todos os espaços e o varchar so o tamanho q inserir
+valor decimal(10,2)                -- numero decimal, o 10 é o default caso nao seja especificado
 );
 
 
 create table matricula (
-id_matricula int primary key,
+id_matricula int primary key,     
 id_cliente int,
 id_curso int,
 valor decimal(10,2),
-data datetime
+data datetime                      -- aqui é a data e a hora
 );
