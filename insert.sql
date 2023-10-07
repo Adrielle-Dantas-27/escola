02168aac19b865f66c11b40bbb539b00a52fc8ae
Adrielle-Dantas-27 <Adrielledsilva@gmail.com>
2023-10-07 13:16:07 -0300

    added comments

--- insert.sql
+++ insert.sql
@@ -1,15 +1,19 @@
-insert into cliente(id_cliente, nome) values
+-- insere dados na tabela cliente
+insert into cliente(id_cliente, nome) values  
  (1, 'Adrielle'),
  (2, 'Victor'),
  (3, 'Adriano');
  
- insert into curso(id_curso, nomecurso, valor) values 
- (1, 'Java', 1000),
+-- insere dados na tabela curso
+-- todos esses dados precisam estar em ordem conforme foi criado as coluna da tabela q queira add os dados
+ insert into curso(id_curso, nomecurso, valor) values  
+ (1, 'Java', 1000),    
  (2, 'Excel', 500),
  (3, 'Python', 1500),
  (4, 'SQL', 2000);
  
- insert into matricula(id_matricula, id_cliente, id_curso, data, valor) values 
+ -- insere dados na tabela matricula
+ insert into matricula(id_matricula, id_cliente, id_curso, data, valor) values     
  (1, 1, 1, '2023-10-06 10:12:01', '1000'),
  (2, 3, 2, '2023-10-06 12:17:01', 500.00),
  (3, 2, 3, '2023-10-06 12:20:01', 1500),
@@ -21,4 +25,4 @@ insere uma nova matricula, porem utilizando um comando de select para obter o va
 */
  insert into matricula(id_matricula, id_cliente, id_curso, data, valor) values 
  (5, 3, 4, '2023-10-07 19:30:20', (select valor from curso where id_curso = 4));
- 
+ -- serve atualizar um novo dado da tabela
