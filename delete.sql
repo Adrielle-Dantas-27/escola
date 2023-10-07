2fcb7c1e283fa12ec2c05c18fb752ae037bf86ef
Adrielle-Dantas-27 <Adrielledsilva@gmail.com>
2023-10-07 13:34:13 -0300

    added comments

--- delete.sql
+++ delete.sql
@@ -1,2 +1,7 @@
-delete from matricula
-where id_matricula = 5;
+/*
+deleta um registro da tabela matricula onde o id_matricula = 5
+where -> filtra pelo identificador unico da matricula(id_matricula)
+OBS: especificar o que deseja deletar para nao correr o risco de deletar tudo e nao esquecer de por o where nem no update nem no delete
+*/
+delete from matricula 
+where id_matricula = 5; 
