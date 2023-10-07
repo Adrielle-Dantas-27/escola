8a9a98d0b5c275d74f6368b3ec647260e2963808
Adrielle-Dantas-27 <Adrielledsilva@gmail.com>
2023-10-07 13:25:36 -0300

    added commited

--- update.sql
+++ update.sql
@@ -1,4 +1,9 @@
+ /*
+ atualiza o dado da coluna valor para 2500 na tabela curso
+ set -> indica a(s) coluna(s) que deve(rão) ser(em) atualizada(s)
+ where -> filtra apenas o identificador unico do curso, para atualizar somente uma linha!!!!
+ */
 update curso 
-set valor = '2500'
-where id_curso = 4;
+set valor = '2500', data = '2023-10-10'
+where id_curso = 4;    
 
