b65e5a353c15e971d98c2aad7a172a683f9fa162
Adrielle-Dantas-27 <Adrielledsilva@gmail.com>
2023-10-07 13:38:47 -0300

    added comments

--- select.sql
+++ select.sql
@@ -1,5 +1,19 @@
-select* from cliente;
+/*
+seleciona todas as colunas da tabela cliente e exibe todos os registos da tabela
+*/
+select * from cliente;  
 
-select* from curso;
+/*
+seleciona apenas a coluna nome da tabela cliente e exibe todos os registos 
+*/
+select nome from cliente;  
 
-select* from matricula;
+/*
+seleciona todas as colunas da tabela curso e exibe todos os registos da tabela
+*/
+select * from curso;     
+
+/*
+seleciona todas as colunas da tabela matricula e exibe todos os registos da tabela
+*/
+select * from matricula;
