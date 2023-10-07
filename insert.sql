9f5fb15f76036597462b2c4f16b2b8285e2ebe42
Adrielle-Dantas-27 <Adrielledsilva@gmail.com>
2023-10-07 12:23:02 -0300

    insert data into tables

---
+++ insert.sql
@@ -0,0 +1,18 @@
+insert into cliente(id_cliente, nome) values
+ (1, 'Adrielle'),
+ (2, 'Victor'),
+ (3, 'Adriano');
+ 
+ insert into curso(id_curso, nomecurso, valor) values 
+ (1, 'Java', 1000),
+ (2, 'Excel', 500),
+ (3, 'Python', 1500),
+ (4, 'SQL', 2000);
+ 
+ insert into matricula(id_matricula, id_cliente, id_curso, data, valor) values 
+ (1, 1, 1, '2023-10-06 10:12:01', '1000'),
+ (2, 3, 2, '2023-10-06 12:17:01', 500.00),
+ (3, 2, 3, '2023-10-06 12:20:01', 1500),
+ (4, 1, 4, '2023-10-06 13:30:20', 2000);
+ 
+ 
